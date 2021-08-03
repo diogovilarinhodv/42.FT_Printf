a43836e6da53711ab0d3a503cbaa590a41a18a58
digui13 <www.eee.az@gmail.com>
2021-08-03 05:13:57 +0100

    Add files via upload

---
+++ Libft/libft.h
@@ -0,0 +1,13 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_itoa(int n);
+int		ft_atoi(const char *nptr);
+
+#endif
