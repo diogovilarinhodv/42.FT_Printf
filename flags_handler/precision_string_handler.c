07690e22f6ed9e0ff008e26b2b79207a830e91ea
digui13 <www.eee.az@gmail.com>
2021-08-09 18:27:00 +0100

    Add files via upload

--- flags_handler/precision_string_handler.c
+++ flags_handler/precision_string_handler.c
@@ -2,18 +2,15 @@
 
 void	precision_string_handler(t_flag_count fc, int str_len, char *str)
 {
-	str_len = ft_strlen(str);
+	str_len = ft_strlen(str) - fc.precision;
 	if (fc.dot > 0)
 	{
-		if ((str_len - 1) < fc.precision)
+		if (str_len)
+			*(str + fc.precision) = '\0';
+		else
 		{
-			*(str + (str_len - 1)) = '\n';
-			*(str + (str_len - 1) + 1) = '\0';
-		}
-		else if (str_len)
-		{
-			*(str + fc.precision) = '\n';
-			*(str + fc.precision + 1) = '\0';
+			str_len = ft_strlen(str);
+			*(str + str_len) = '\0';
 		}
 	}
 }
