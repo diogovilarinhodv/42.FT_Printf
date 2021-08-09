c9f9b46b25b6871eed69013f58fe3d2b454dd6a8
digui13 <www.eee.az@gmail.com>
2021-08-09 18:26:02 +0100

    Add files via upload

--- utils/count_flag.c
+++ utils/count_flag.c
@@ -1,8 +1,21 @@
 #include "../ft_printf.h"
 
+static void	width_flag_handler(t_flag_count *fc, char *ch)
+{
+	fc->width *= 10;
+	fc->width += ft_atoi(ch);
+}
+
+static void	precision_flag_handler(t_flag_count *fc, char *ch)
+{
+	fc->precision *= 10;
+	fc->precision += ft_atoi(ch);
+}
+
 void	count_flag(char c, t_flag_count *fc)
 {
 	char	*ch;
+
 	ch = malloc(2);
 	*ch = c;
 	*(ch + 1) = '\0';
@@ -19,14 +32,8 @@ void	count_flag(char c, t_flag_count *fc)
 	else if (*ch == '.')
 		fc->dot++;
 	else if ('0' <= *ch && *ch <= '9' && fc->dot == 0)
-	{
-		fc->width *= 10;
-		fc->width += ft_atoi(ch);
-	}
+		width_flag_handler(fc, ch);
 	else if (('0' <= *ch && *ch <= '9') && fc->dot > 0)
-	{
-		fc->precision *= 10;
-		fc->precision += ft_atoi(ch);
-	}
+		precision_flag_handler(fc, ch);
 	free(ch);
 }
